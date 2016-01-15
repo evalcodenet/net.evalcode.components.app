0a8e766f7e05e66162be53c3d1df24aeb43a745b
Carsten Schipke <carsten.schipke@gmail.com>
2016-01-16 00:28:52 +0800

    Cleanup.

--- config/environment.php
+++ config/environment.php
@@ -1,16 +1,7 @@
 <?php
 
 
-  $root=dirname(dirname(__DIR__));
-
-
   \Components\Environment::push(
-    \Components\Environment::create()
-      ->setPathApplication("$root/app")
-      ->setPathComponents($root)
-      ->setPathConfig("$root/app/config")
-      ->setPathResource("$root/app/resource")
-      ->setUriComponents('/')
-      ->setUriResource('/resource')
+    \Components\Environment::create()->setUriComponents('/')
   );
 ?>
